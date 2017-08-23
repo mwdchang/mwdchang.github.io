02ddd5f5663b62a75e892bfe477cdf63145f3009
Daniel <mwdchang@gmail.com>
2017-08-23 07:54:19 -0400

    new theme and design

---
+++ data.js
@@ -0,0 +1,10 @@
+const GITHUB_ROOT = 'https://github.com/mwdchang/'
+
+const SITE_DATA = {
+  nebulis: {
+     title: 'Nebulis validator',
+     desc: 'Validates Nebulis model transform',
+     img: '/images/g_cssball.png',
+     url: GITHUB_ROOT + 'schema-editor'
+  }
+}
