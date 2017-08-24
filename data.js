3d3f786324151937f9afdc39c24ec40926248414
Daniel <mwdchang@gmail.com>
2017-08-24 00:09:16 -0400

    filling out templates

--- data.js
+++ data.js
@@ -12,5 +12,48 @@ const SITE_DATA = {
      desc: 'A flexible, API driven stacking library',
      img: 'https://raw.githubusercontent.com/mwdchang/stckr/master/stckr.gif',
      url: GITHUB_ROOT + 'stckr'
+  },
+  fisheye: {
+    title: 'Fisheye shader',
+    desc: 'WebGL Fisheye effect',
+    img: 'images/g_fisheye.png',
+    url: 'https://github.com/mwdchang/Demos/tree/master/fisheye'
+  },
+  polaroid: {
+    title: 'Polaroid',
+    desc: 'Image processing and filters',
+    img: 'images/g_polaroid.png',
+    url: 'https://github.com/mwdchang/Demos/tree/master/polaroidComposite'
+  },
+  thesis: {
+    title: 'My thesis project',
+    desc: 'VehicleVis: combinging text visualiation with 3D graphcis',
+    img: 'images/g_thesis.png',
+    url: 'https://github.com/mwdchang/VehicleVis'
+  },
+  grid: {
+    title: 'Grid reduction experiment',
+    desc: 'Reduce grid noise by only showing localized context',
+    img: 'images/g_grid.png',
+    url: 'http://vialab.science.uoit.ca/mwdchang/projects/grid/testGrid.html'
+  },
+  clock: {
+    title: 'Clock',
+    desc: 'Abstract and artistic clocks written in D3',
+    img: 'images/g_clock.png',
+    url: 'https://github.com/mwdchang/VisDemos/tree/master/clock'
+  },
+  dbscan: {
+    title: 'DBSCAN',
+    desc: 'DBSCAN algorithm and visualization',
+    img: 'images/dbscan.png',
+    url: 'https://github.com/mwdchang/dbscanVis'
+  },
+  arc: {
+    title: 'Arc diagram',
+    desc: 'Using arc diagram in place of venn diagrams',
+    img: 'images/g_arcs.png',
+    url: 'https://github.com/mwdchang/VisDemos/tree/master/arcs'
   }
+
 }
