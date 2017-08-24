bff4d22c18abd41da66cd7eea9ec1ca738bdf5cc
Daniel <mwdchang@gmail.com>
2017-08-23 23:13:51 -0400

    refine vue component

--- data.js
+++ data.js
@@ -3,8 +3,14 @@ const GITHUB_ROOT = 'https://github.com/mwdchang/'
 const SITE_DATA = {
   nebulis: {
      title: 'Nebulis validator',
-     desc: 'Validates Nebulis model transform',
-     img: '/images/g_cssball.png',
+     desc: 'Edit and validates Nebulis transform models. ',
+     img: 'images/g_nebulis.png',
      url: GITHUB_ROOT + 'schema-editor'
+  },
+  stckr: {
+     title: 'Stckr',
+     desc: 'A flexible, API driven stacking library',
+     img: 'https://raw.githubusercontent.com/mwdchang/stckr/master/stckr.gif',
+     url: GITHUB_ROOT + 'stckr'
   }
 }
